32564cfd5f4c2a0b711d6a490d4e44483b0808e0
Administrator <1807103336@qq.com>
2018-08-07 13:29:36 +0800

    log latitude longtitude

--- month6_predict/log_transform_latitude_latitude.py
+++ month6_predict/log_transform_latitude_latitude.py
@@ -12,3 +12,20 @@ import numpy as np
 
 train_data = pd.read_csv('./final_process_train_6_dnn.csv')
 test_data = pd.read_csv('./final_process_test_6_dnn.csv')
+
+train_data['longitude'] = np.log1p(train_data['longitude'])
+train_data['latitude'] = np.log1p(train_data['latitude'])
+test_data['longitude'] = np.log1p(test_data['longitude'])
+test_data['latitude'] = np.log1p(test_data['latitude'])
+
+
+print(train_data.head())
+print(test_data.head())
+
+# train_data =pd.get_dummies(train_data)
+# test_data = pd.get_dummies(test_data)
+#
+# train_data.to_csv('./standard_longitude_latitude/standard_log_lat_train.csv',index=False)
+# test_data.to_csv('./standard_longitude_latitude/standard_log_lat_test.csv',index=False)
+
+
