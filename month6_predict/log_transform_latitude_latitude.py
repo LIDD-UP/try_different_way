4c02e0028c85ea8a38b22283f4b8d810cc03b2e8
Administrator <1807103336@qq.com>
2018-08-07 13:26:14 +0800

    commit data

--- month6_predict/log_transform_latitude_latitude.py
+++ month6_predict/log_transform_latitude_latitude.py
@@ -4,3 +4,11 @@
 @file: log_transform_latitude_latitude.py
 @time: 2018/8/7
 """
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+
+train_data = pd.read_csv('./final_process_train_6_dnn.csv')
+test_data = pd.read_csv('./final_process_test_6_dnn.csv')
