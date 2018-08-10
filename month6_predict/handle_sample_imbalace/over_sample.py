18a213597e543f76eb7c07d0968e72204d87df87
Administrator <1807103336@qq.com>
2018-08-10 15:50:55 +0800

    oversample and undersample all including dnn and xgboost

--- month6_predict/handle_sample_imbalace/over_sample.py
+++ month6_predict/handle_sample_imbalace/over_sample.py
@@ -10,30 +10,60 @@ import matplotlib.pyplot as plt
 from random import sample
 from imblearn.over_sampling import SMOTE
 
-train_data = pd.read_csv('../standard_longitude_latitude/standard_log_lat_train.csv')
-test_data = pd.read_csv('../standard_longitude_latitude/standard_log_lat_test.csv')
+over_sample_data = pd.read_csv('../standard_longitude_latitude/standard_log_lat_train.csv')
+# test_data = pd.read_csv('../standard_longitude_latitude/standard_log_lat_test.csv')
+# train_data['bedrooms'] = train_data['bedrooms'].astype(int)
+# train_data['bedrooms'] = train_data['bedrooms'].astype(str)
 
-# 将样本分为平衡特征与需要过采样的特征即label
-feature = train_data[['longitude','latitude','price','bedrooms','daysOnMarket']]
-label = train_data['buildingTypeId']
-print(feature.shape)
-print(label.shape)
-
-
-smoter = SMOTE(random_state=0)
-feature,label = smoter.fit_sample(feature,label)
-
-print(feature.shape)
-print(label.shape)
-feature = pd.DataFrame(feature,columns=['longitude','latitude','price','bedrooms','daysOnMarket'])
-label = pd.DataFrame(label,columns=['buildingTypeId'])
-
-over_sample_data = pd.concat((feature,label),axis=1)
-print(over_sample_data.shape)
-print(over_sample_data.head())
-print(over_sample_data['buildingTypeId'].value_counts())
+# print(train_data.dtypes)
+# # 将样本分为平衡特征与需要过采样的特征即label
+# feature = train_data[['longitude','latitude','price','bedrooms','daysOnMarket']]
+# label = train_data['buildingTypeId']
+# print(feature.shape)
+# print(label.shape)
+#
+#
+# smoter = SMOTE(random_state=0)
+# feature,label = smoter.fit_sample(feature,label)
+#
+# print(feature.shape)
+# print(label.shape)
+# feature = pd.DataFrame(feature,columns=['longitude','latitude','price','bedrooms','daysOnMarket'])
+# label = pd.DataFrame(label,columns=['buildingTypeId'])
+#
+# over_sample_data = pd.concat((feature,label),axis=1)
+# print(over_sample_data.shape)
+# print(over_sample_data.head())
+# over_sample_data = over_sample_data[over_sample_data.bedrooms.isin([1,2,3,4,5])] # 去掉bedrooms不为整数的点；
+# print(over_sample_data['buildingTypeId'].value_counts())
+#
+#
+# print(over_sample_data['bedrooms'].value_counts())
+# # over_sample_data.to_csv('over_sample_buildingTypeId.csv',index=False)
 
 # 对bedrooms 进行过采样
-print(over_sample_data['bedrooms'].value_counts())
+# 简单的过采样方法：
+print(over_sample_data.head())
+over_sample_data['bedrooms'] = over_sample_data['bedrooms'].astype(str)
+feature_bedrooms = over_sample_data[['longitude','latitude','price','buildingTypeId','daysOnMarket']]
+label_bedrooms = over_sample_data['bedrooms']
+smoter2 = SMOTE()
+feature_bedrooms,label_bedrooms = smoter2.fit_sample(feature_bedrooms,label_bedrooms)
+print(feature_bedrooms.shape)
+print(label_bedrooms.shape)
+feature_bedrooms = pd.DataFrame(feature_bedrooms,columns=['longitude','latitude','price','buildingTypeId','daysOnMarket'])
+label_bedrooms = pd.DataFrame(label_bedrooms,columns=['bedrooms'])
+
+over_sample_data_bedrooms = pd.concat((feature_bedrooms,label_bedrooms),axis=1)
+print(over_sample_data_bedrooms.shape)
+print(over_sample_data_bedrooms.head())
+# over_sample_data_bedrooms = over_sample_data_bedrooms[over_sample_data_bedrooms.bedrooms.isin([1,2,3,4,5])] # 去掉bedrooms不为整数的点；
+print(over_sample_data_bedrooms['bedrooms'].value_counts())
+
+over_sample_data_bedrooms.to_csv('over_sample_bedrooms.csv',index=False)
+
+
+
+
 
 
---
+++ month6_predict/handle_sample_imbalace/under_sample/under_sample_bedrooms/xgboost_test.py
@@ -0,0 +1,109 @@
+#-*- coding:utf-8 _*-  
+""" 
+@author:Administrator
+@file: xgboost_test.py
+@time: 2018/8/10
+"""
+import numpy as np
+
+np.set_printoptions(suppress=False)
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.stats import skew
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
+import os
+
+# pandas 的显示设置函数：
+mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体 SimHei为黑体
+mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+pd.set_option('max_columns', 200)
+pd.set_option('display.width', 1000)
+
+# train_data = pd.read_csv('./final_process_train_6.csv')
+# test_data = pd.read_csv('./final_process_test_6.csv')
+
+test_data_path = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+train_data = pd.read_csv('./under_sample_bedrooms.csv')
+test_data = pd.read_csv(test_data_path +'/standard_longitude_latitude/standard_log_lat_test.csv')
+
+train = train_data.drop(columns='daysOnMarket')
+test = test_data.drop(columns='daysOnMarket')
+
+
+train_label = train_data['daysOnMarket']
+test_label = np.expm1(test_data['daysOnMarket'])
+
+
+print(train.head())
+print(test.head())
+
+from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
+# print(help(XGBRegressor))
+
+# 寻找超参数
+params = {
+          # 'n_estimators': [100,300,500,1000,5000],# 300
+          'max_depth':[x for x in range(5,6,1)],#5
+          # 'max_depth':[x for x in range(3,10,1)],#5
+          # 'learning_rate':[0.001,0.01,0.05,0.1,0.3,0.5,0.7,0.9], # 0.3
+          # 'reg_alpha':[1e-5,1e-2,0.1,1,100],#1
+          # 'gamma':[x for x in range(0,10,1)],#0
+          # 'min_child_weight':[x for x in range(1,10,1)],# 5
+          #   'subsample':[x for x in np.arange(0,1,0.01)], # 0.65
+          }
+'''
+   eta –> learning_rate
+   lambda –> reg_lambda
+   alpha –> reg_alpha
+'''
+
+grid = GridSearchCV(estimator=XGBRegressor(),param_grid=params,scoring='neg_mean_absolute_error')
+
+# 训练
+grid.fit(train,train_label)
+# print(len(grid.cv_results_.values()))
+# print(help(grid.))
+# 打印最好参数和最好的得分值
+print('best_params',grid.best_params_)
+print('best_scoring',grid.best_score_)
+print('best:%f use:%r'%(grid.best_score_,grid.best_params_))
+for params, mean_score, scores in grid.grid_scores_:
+    print("%f (%f) with: %r" % (scores.mean(), scores.std(), params))
+model = grid.best_estimator_
+print(model)
+
+
+
+# 预测
+preds = np.expm1(model.predict(test))
+print('error',mean_absolute_error(test_label,preds))
+print('pred_mean',preds.mean())
+print('true_mean',test_label.mean())
+
+# 画图
+plt.figure(figsize=(100,100))
+plt.plot(preds[0:100],c='blue',label='pred')
+plt.plot(test_label,c='red',label='true')
+plt.title("RandomForest preds and true daysOnMarket distribution circumstance")
+plt.legend()
+plt.show()
+
+'''
+error 8.511474361749844
+pred_mean 15.454598
+true_mean 18.470039946737682
+'''
+
+'''
+standard_longitude_latitude 
+error 8.69978157023139
+pred_mean 15.880932
+true_mean 18.470039946737682
+'''
+
+
+
