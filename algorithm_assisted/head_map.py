359499d63a605d227043c28469956399df743137
Administrator <1807103336@qq.com>
2018-08-28 17:05:45 +0800

    use kaggle dnn to predict  data is the previous data

---
+++ algorithm_assisted/head_map.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 _*-  
+""" 
+@author:Administrator
+@file: head_map.py
+@time: 2018/8/28
+"""
+# k = 40 #number of variables for heatmap
+# cols = corrmat.nlargest(k, 'daysOnMarket')['daysOnMarket'].index
+# cm = np.corrcoef(data[cols].values.T)
+# sns.set(font_scale=1.25)
+# hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+# plt.show()
+# '''
