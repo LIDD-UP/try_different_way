29cb4f9d000e06b6da3daa75ba01466409bd9a36
Administrator <1807103336@qq.com>
2018-11-21 17:05:00 +0800

    continue to test remove ratio

--- first_reporter_task_one_week_finish/test_treb/merge_data_bak/predict_analysis_calculate_ratio.py
+++ first_reporter_task_one_week_finish/test_treb/merge_data_bak/predict_analysis_calculate_ratio.py
@@ -7,59 +7,60 @@
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 
-data = pd.read_csv('merge_data_auto_ml.csv')
+# data = pd.read_csv('merge_data_auto_ml.csv')
 # data = pd.read_csv('merge_data_auto_ml_xgboost.csv')
-# data = pd.read_csv('../input/treb_toronto_9.csv')
+data = pd.read_csv('../input/treb_toronto_10.csv')
 
 
 
-# data_10 = []
-# data_20 = []
-# data_30 = []
-# data_more = []
-#
-# # data = data.drop(columns=['index'])
-#
-# test_column = 'projectDaysOnMarket'
+data_10 = []
+data_20 = []
+data_30 = []
+data_more = []
+
+# data = data.drop(columns=['index'])
+
+test_column = 'projectDaysOnMarket'
 # test_column = 'predictions'
 
 
-# for i in range(len(data)):
-#     print(i)
-#     if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=10:
-#         data_10.append(i)
-#     if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) > 10 and abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=20:
-#         data_20.append(i)
-#     if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) > 20 and abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=30:
-#         data_30.append(i)
-#     if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) >30:
-#         data_more.append(i)
-#
-# print(len(data_10)/len(data))
-# print(len(data_20)/len(data))
-# print(len(data_30)/len(data))
-# print(len(data_more)/len(data))
-# print(mean_absolute_error(data[test_column],data['daysOnMarket']))
+for i in range(len(data)):
+    print(i)
+    if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=10:
+        data_10.append(i)
+    if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) > 10 and abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=20:
+        data_20.append(i)
+    if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) > 20 and abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) <=30:
+        data_30.append(i)
+    if abs(data.iloc[i][test_column] - data.iloc[i]['daysOnMarket']) >30:
+        data_more.append(i)
 
-# def compute_ratio2(data):
-#     test_column = 'predictions'
-#     data_len = len(data)
-#     print(len(data[abs(data.predictions - data.daysOnMarket) <= 10])/data_len)
-#     print(len(data[abs(data.predictions - data.daysOnMarket).all() > 10 and abs(
-#                 data.predictions - data.daysOnMarket).all() <= 20])/data_len)
-#     print(len(data[abs(data.predictions - data.daysOnMarket) > 20 and abs(
-#                 data.predictions - data.daysOnMarket) <= 30])/data_len)
-#     print(len(data[abs(data.predictions - data.daysOnMarket) > 30])/data_len)
-#     print(mean_absolute_error(data[test_column], data['daysOnMarket']))
+print(len(data_10)/len(data))
+print(len(data_20)/len(data))
+print(len(data_30)/len(data))
+print(len(data_more)/len(data))
+print(mean_absolute_error(data[test_column],data['daysOnMarket']))
 
-if __name__ == '__main__':
-    # compute_ratio2(data)
+def compute_ratio2(data):
+    test_column = 'predictions'
+    data_len = len(data)
+    print(len(data[abs(data.predictions - data.daysOnMarket) <= 10])/data_len)
+    print(len(data[abs(data.predictions - data.daysOnMarket).all() > 10 and abs(
+                data.predictions - data.daysOnMarket).all() <= 20])/data_len)
+    print(len(data[abs(data.predictions - data.daysOnMarket) > 20 and abs(
+                data.predictions - data.daysOnMarket) <= 30])/data_len)
+    print(len(data[abs(data.predictions - data.daysOnMarket) > 30])/data_len)
+    print(mean_absolute_error(data[test_column], data['daysOnMarket']))
+
+# if __name__ == '__main__':
+#     # compute_ratio2(data)
 
 
 
 
 
 '''
+9月份情况；
 0.3420238095238095
 0.2695238095238095
 0.07547619047619047
@@ -76,11 +77,12 @@ my predictions
 24.81238204908187
 '''
 '''
-0.3322876447876448
-0.2548262548262548
-0.16011100386100385
-0.2527750965250965
-22.444079292096685
+10月份的情况
+0.36163372231483687
+0.29328411526553944
+0.09085496546796856
+0.25422719695165513
+27.666706358656825
 '''
 
 
