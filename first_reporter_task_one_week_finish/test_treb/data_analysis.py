788fd654cba060a13301ad6e467c713ea59296a3
Administrator <1807103336@qq.com>
2018-11-22 16:28:18 +0800

    测试出新的特征；

--- first_reporter_task_one_week_finish/test_treb/data_analysis.py
+++ first_reporter_task_one_week_finish/test_treb/data_analysis.py
@@ -9,13 +9,84 @@ import missingno as msno
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data_train = pd.read_csv('./input/test_treb_month_78.csv')
-data_train_all = pd.read_csv('./input/treb_all_column_month_3to8.csv')
+data_train = pd.read_csv('./input/treb_toronto_all.csv')
+# data_train_all = pd.read_csv('./input/treb_all_column_month_3to8.csv')
+#
+# data_test = pd.read_csv('./input/treb_test_month_9.csv')
+# data_test_all = pd.read_csv('./input/treb_test_all_column_month_9.csv')
 
-data_test = pd.read_csv('./input/treb_test_month_9.csv')
-data_test_all = pd.read_csv('./input/treb_test_all_column_month_9.csv')
-
-msno.bar(data_test_all)
+data = data_train.iloc[:,150:200]
+msno.bar(data)
 plt.tight_layout()
+# plt.tick_params(labelsize=11)
 plt.show()
+
+
+'''
+'province',
+'city',
+'address',
+'postcode',
+'longitude',
+'latitude',
+'price',
+'buildingTypeId',
+'tradeTypeId',
+'listingDate',
+'style',
+'community',
+'airConditioning',
+'washrooms',
+'basement1',
+'familyRoom',
+'fireplaceStove',
+'heatSource',
+'garageType',
+'kitchens',
+'parkingSpaces',
+'parkingIncluded',
+'rooms',
+'waterIncluded',
+'totalParkingSpaces',
+'district',
+'projectDaysOnMarket',
+'daysOnMarket',
+'bedrooms',
+'ownerShipType',
+'propertyTypeId',
+'propertyType',
+'bathroomTotal',
+'treb',
+'municpCode',
+'communityCode',
+'municipalityDistrict',
+'municipality',
+'pixUpdatedDate',
+'remarksForClients',
+'cableTVIncluded',
+'cacIncluded',
+'commonElementsIncluded',
+'directions',
+'extras',
+'fireplaceStove',
+'heatType',
+'kitchens',
+'parkingSpaces',
+'parkingIncluded',
+'listBrokerage',
+'streetName',
+'streetDirection',
+'streetNo',
+'streetAbbreviation',
+'washroomsType1Pcs',
+'washroomsType1',
+'exterior1',
+'privateEntrance',
+
+
+
+
+
+
+'''
 
